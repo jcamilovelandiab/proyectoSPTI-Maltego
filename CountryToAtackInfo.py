1bf58726b19edbe5c19b37b803c74baa0faeef63
alejoguzm07 <alejoguzm07@gmail.com>
2019-04-30 17:55:51 -0500

    Sacando informacion, no esta completa

--- CountryToAtackInfo.py
+++ CountryToAtackInfo.py
@@ -5,7 +5,7 @@ import time
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from PIL import ImageTk, Image
+#from PIL import ImageTk, Image
 import time
 import tkinter as tk
 import sys
@@ -17,36 +17,19 @@ repoName = "hola"
 
 chrome_options = Options()
 driver = None
-try:
-    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe"), chrome_options=chrome_options)
-    driver.get("https://otx.alienvault.com")
-except Exception as e:
-    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options)
-    driver.get("https://otx.alienvault.com")
-
-'''
-LOGIN, CUENTA FALSA EN OTX
-'''
-try:
-    formaLogin = driver.find_element_by_xpath("/html/body/app-root/div[1]/div[1]/app-home/section[1]/div/div/div[2]/div/ul/li[2]/button")
-    formaLogin.click()
-except Exception as e:
-    formaLogin = driver.find_element_by_xpath("/html/body/app-root/div[1]/div[1]/app-home/section[1]/div/div/div[2]/div/ul/li[2]/button")
-    formaLogin.click()
-usuario = "osintmaltego"
-contraseña = "osint123"
-username = driver.find_element_by_id("id_login")
-password = driver.find_element_by_id("id_password")
-username.send_keys(usuario)
-password.send_keys(contraseña)
-botonLogin = driver.find_element_by_id("loginBtn")
-botonLogin.click()
 
 '''
 BUSQUEDA DEL PAIS
 '''
-rutaPais = "https://otx.alienvault.com/browse/pulses?q=country:%20" + "Colombia"
-driver.get(rutaPais)
+
+try:
+    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe"), chrome_options=chrome_options)
+    rutaPais = "https://otx.alienvault.com/browse/pulses?q=country:%20" + "Colombia"
+    driver.get(rutaPais)
+except Exception as e:
+    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options)
+    rutaPais = "https://otx.alienvault.com/browse/pulses?q=country:%20" + "Colombia"
+    driver.get(rutaPais)
 
 '''
 CANTIDAD DE PULSOS
@@ -72,7 +55,22 @@ urlsPulsos = []
 for pulso in pulsos:
     urlsPulsos.append("https://otx.alienvault.com/" + pulso.get_attribute("id").replace("-", "/"))
 for pulso in urlsPulsos:
+    infoPulso = {}
     driver.get(pulso)
     time.sleep(5)
     print(driver.find_element_by_xpath("/html/body/app-root/div[1]/div[1]/app-pulse-detail/div[1]/div/div/div/header/div[1]/h1").text)
+    time.sleep(5)
+    driver.find_element_by_name("DataTables_Table_0_length").click()
+    time.sleep(1)
+    driver.find_element_by_xpath("/html/body/app-root/div[1]/div[1]/app-pulse-detail/div[1]/div/div/div/div/app-pulse-indicators/div/div/div[2]/div/div[1]/div[1]/div/label/select/option[2]").click()
+    time.sleep(3)
+    barraBusqueda = driver.find_element_by_xpath("/html/body/app-root/div[1]/div[1]/app-pulse-detail/div[1]/div/div/div/div/app-pulse-indicators/div/div/div[2]/div/div[1]/div[2]/div/label/input")
+    barraBusqueda.send_keys("URL")
+    barraBusqueda.send_keys(Keys.END)
+    indicadores1 = driver.find_elements_by_xpath("//tbody/*[@class='show-row no-footer odd']")
+    indicadores2 = driver.find_elements_by_xpath("//tbody/*[@class='show-row no-footer even']")
+    for fila in indicadores1:
+        info = fila.find_element_by_xpath("//a").get_property("href")
+        print(info)
+    #for fila in indicadores2:
 #driver.quit()
